c5a605b9f45214f2fe11a8a4c1ee7101315f9c11
volkov lev <volkov.lr@phystech.edu>
2026-02-04 16:44:11 +0300

    fix tests

--- tests/test_base.py
+++ tests/test_base.py
@@ -3,31 +3,26 @@ import pytest
 from satellite_system.coordinator import Coordinator
 coordinator = Coordinator()
 
-EARTH_RADIUS_M = 6371e3
+EARTH_RADIUS_KM = 6371.0
 
 def analytic_coverage(satel: int, altitude: float, angle: float) -> float:
   """Analytical assessment of Earth's satellite coverage.
 
   Args:
       satel (int): number of satellites
-      altitude (float): orbit altitude, meters
-      fov_deg (float): full field of view, degrees
+      altitude (float): orbit altitude, km
+      fov_deg (float): half the viewing angle, degrees
 
   Returns:
       float: Earth coverage
   """
-  R = EARTH_RADIUS_M
+  R = EARTH_RADIUS_KM
 
   theta = math.radians(angle)
 
-  cos_psi = (R / (R + altitude)) * math.sin(theta)
+  betta = math.asin((R + altitude) * math.sin(theta) / R)
 
-  psi = math.asin(cos_psi)
-
-  area_one = 2 * math.pi * R**2 * (1 - math.cos(psi - theta))
-  earth_area = 4 * math.pi * R**2
-
-  return (satel * area_one) / earth_area
+  return satel * 0.5 * (1 - math.cos(betta - theta))
 
 def coverage(height, orb_inclin, longitude_asc, count_orb, count_sat, angle, res):
   """
@@ -37,27 +32,27 @@ def coverage(height, orb_inclin, longitude_asc, count_orb, count_sat, angle, res
 
   coverage_code = coordinator.calculate_coverage(f"{group_number} {res} 2024-01-15 14:30:45.123456".split())
 
-  coverage_analytic = analytic_coverage(count_sat, height * 1000, angle)
+  coverage_analytic = analytic_coverage(count_sat, height, angle)
 
-  error = (abs(coverage_code - coverage_analytic) / coverage_code) * 100
+  error = (abs(coverage_code - coverage_analytic) / coverage_analytic) * 100
 
   print(f"Coverage (code): {coverage_code * 100:.3g}%")
   print(f"Coverage (test): {coverage_analytic * 100:.3g}%")
   print(f"Error: {error}%")
 
-  assert error <= 100
+  assert error <= 5
 
 def test_coverage_1():
-  coverage(500, 45, 45, 4, 16, 20, 4)
+  coverage(500, 45, 45, 1, 1, 40, 4)
 
 def test_coverage_2():
-  coverage(300, 20, 12, 1, 1, 20, 3)
+  coverage(600, 20, 12, 1, 1, 50, 4)
 
 def test_coverage_3():
-  coverage(700, 70, 3, 2, 8, 20, 4)
+  coverage(700, 70, 3, 1, 1, 30, 4)
 
 def test_coverage_4():
-  coverage(1000, 54, 124, 3, 6, 20, 4)
+  coverage(1000, 54, 124, 1, 1, 40, 4)
 
 def test_coverage_5():
-  coverage(600, 25, 350, 4, 16, 20, 4)
+  coverage(800, 25, 350, 1, 1, 40, 4)
